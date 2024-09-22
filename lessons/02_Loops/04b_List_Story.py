18c8cc9fa0d78cc4c17fb14fa5b2894a89ba7ee6
Anneliese Pimienta <162831580+anneliesepimienta@users.noreply.github.com>
2024-09-22 19:59:29 +0000

    code of septemebr,22/2024

--- lessons/02_Loops/04b_List_Story.py
+++ lessons/02_Loops/04b_List_Story.py
@@ -4,12 +4,13 @@ Use the words in the list to create a story.
 Use indexing to get words from the list, then 
 append them to the story
 
-"""
-
+            0       1       2   3       4       5       6      7    8
+"""            
 words = ['Once', '👦', 'upon', '🐕', 'park', 'met', 'with', 'a', 'the', 
-    'time', 'to', 'who', '🐈', '👧', 'and', 'went', 'had', 'play', '⚽.', 'they']
+    'time', 'to', 'who', '🐈', '👧', 'and', 'went', 'had', 'play', '⚽.', 
+    'they','at']
 
-story = []
+story = [words[0],words[2],words[7],words[9],words[7],words[13],words[14],words[1],words[15],words[10],words[17],words[20],words[8],words[4],words[10],words[17],words[18],words[19],words[16],words[7],words[3],words[14],words[12],words[11],words[17],words[6],words[1],words[14],words[13]]
 
 # Create a story using the words in the list
 
--- lessons/02_Loops/06_Fizz_Buzz_Badgers.py
+++ lessons/02_Loops/06_Fizz_Buzz_Badgers.py
@@ -17,7 +17,7 @@ Your program should print 4 badgers.
 """
 
 
-for i in range(1, 31): # Change only this line
+for i in [5,10,20,25,35,40,50,55,65,70,80,85,95,100]: # Change only this line
 
     # Don't change anything below this line
     if i % 15 == 0:
