c79dd95274bb4f659c6d75c3e73120b4382c4194
Anneliese Pimienta <162831580+anneliesepimienta@users.noreply.github.com>
2025-01-26 20:56:46 +0000

    code of january 26, 2025

--- projects/06_Projects/hotel_mangment.py
+++ projects/06_Projects/hotel_mangment.py
@@ -1,7 +1,31 @@
 """hi"""
+"""max # of nights 5
+max # of rooms 5
+if not enough rooms send guest away
+each night $100.00
+room service $20.00
+clean rooms comes with visit (tips $5.00)
+check in interactive
+when guest wants something or questions interactive
+max poeple trying to check in 10
+some popele will instead of trying to ask for a room ask questions such as directions,etc
+"""
+room_num = dict()
 
-sERLGk3q4t = {
-    "pi": 65,
-    22: 25,
-    "mwrgoGOMKTEBE,BS'PMBD": "OSFMDLIFJA"
-}
+
+
+check_in = simpledialog.askstring(title="", prompt="Are you here for check in or check out?")
+if check_in == "check in":
+    num_nights= simpledialog.askstring(title="",prompt="ok, and how many nights will you be staying with us?(you cannot stay for more than 5 nights max)").
+    name = simpledialog.askstring(title = "", prompt="what will the name be that your room will be under?")
+         
+#add the answers to the dictionary
+
+
+
+
+
+
+
+if check_in == "check out":
+    
